19b1cc71fd48b40013919e05cc20e7f7535554c9
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-07-07 21:50:08 +0600

    clippath add

--- lib/features/shop/screen/details/details.dart
+++ lib/features/shop/screen/details/details.dart
@@ -1,77 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:travel/common/widgets/cliper/cliper.dart';
 import 'package:travel/utills/constants/colors.dart';
 import 'package:travel/utills/constants/icons.dart';
+
 import 'package:travel/utills/constants/images.dart';
 import 'package:travel/utills/constants/text.dart';
+
 
 class Details extends StatelessWidget {
   const Details({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
 
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ClipPath(
+                // Define a custom path to clip the child
+                clipper: MyClipper(),
+                child: SizedBox(
+                  width: width,
+                  height: width*0.8,
 
-            ClipPath(
-              // Define a custom path to clip the child
-              clipper: MyClipper(),
-              child: Container(
-                width:double.infinity,
-                height: 400,
-
-                child:    Stack(
-                  children: [
-                    Image.asset(RImages.sunamganj,fit: BoxFit.cover,width: double.infinity,height: 350,),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 20,left: 20,right: 20
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          InkWell(
-                              onTap: (){
-                                Get.back();
-                              },
-                              child: Image.asset(RIcons.backArrow,height: 40,)),
-                          const Text("Details",style: TextStyle(fontSize: 20,color: RColores.white),),
-                          InkWell(
-                              onTap: (){
-
-                              },
-                              child: Image.asset(RIcons.bookmark,height: 40,)),
-                        ],
-                      ),
-                    )
-                  ],
+                  child: Image.asset(RImages.sunamganj,fit: BoxFit.cover,),
                 ),
               ),
-            ),
-          ],
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(RTexts.sunamganjResort,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          Text(RTexts.sunamganj)
+                        ],
+                      ),
+                      Image.asset(RIcons.person1,)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(
+                              Icons.location_on_outlined),
+                          Text(RTexts.sunamganj
+                              ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                           Icon(
+                              Icons.star,color: RColores.orangeColor,),
+                          Text("4.7(27)"
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+
+
+                          RichText(text: TextSpan(
+                            children: [
+                              
+                            ]
+                          )
+
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    // Define a custom path to create a stylish clipped shape
-    var path = Path();
-    // path.moveTo(0, size.height * 0.5);
-    path.lineTo(0, size.height);
 
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
-  }
-}
