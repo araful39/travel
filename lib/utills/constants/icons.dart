10925afdb2c09da7425d0c1e1530fe7b06611d81
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-07-14 07:59:40 +0600

    all page done

--- lib/utills/constants/icons.dart
+++ lib/utills/constants/icons.dart
@@ -1,50 +1,36 @@
-class RIcons{
+class RIcons {
+  static const String facebook = "assets/icon/vector/facebook.png";
+  static const String instagram = "assets/icon/vector/instagram.png";
+  static const String twiter = "assets/icon/vector/twiter.png";
 
+  static const String onBoardV1 = "assets/icon/vector/vector1.png";
+  static const String onBoardV2 = "assets/icon/vector/vector2.png";
+  static const String onBoardV3 = "assets/icon/vector/vector3.png";
 
-  static const String facebook="assets/icon/vector/facebook.png";
-  static const String instagram="assets/icon/vector/instagram.png";
-  static const String twiter="assets/icon/vector/twiter.png";
+  static const String email = "assets/icon/vector/email.png";
+  static const String person = "assets/icon/vector/person.png";
+  static const String notification = "assets/icon/vector/notification.png";
 
+  static const String home = "assets/icon/home.png";
+  static const String calender = "assets/icon/calendar.png";
+  static const String search = "assets/icon/search.png";
+  static const String chat = "assets/icon/chat.png";
+  static const String profile = "assets/icon/profile.png";
 
-  static const String onBoardV1="assets/icon/vector/vector1.png";
-  static const String onBoardV2="assets/icon/vector/vector2.png";
-  static const String onBoardV3="assets/icon/vector/vector3.png";
+  static const String person1 = "assets/icon/person1.png";
+  static const String person2 = "assets/icon/person2.png";
+  static const String person3 = "assets/icon/person3.png";
+  static const String person4 = "assets/icon/person4.png";
 
+  static const String bookmark = "assets/icon/bookmark.png";
+  static const String backArrow = "assets/icon/backArrow.png";
 
-  static const String email="assets/icon/vector/email.png";
-  static const String person="assets/icon/vector/person.png";
-  static const String notification="assets/icon/vector/notification.png";
+  static const String profileMask = "assets/images/profileMask.png";
+  static const String trip = "assets/icon/trip.png";
+  static const String settings = "assets/icon/settings.png";
+  static const String version = "assets/icon/version.png";
 
-
-
-  static const String home="assets/icon/home.png";
-  static const String calender="assets/icon/calendar.png";
-  static const String search="assets/icon/search.png";
-  static const String chat="assets/icon/chat.png";
-  static const String profile="assets/icon/profile.png";
-
-  static const String person1="assets/icon/person1.png";
-  static const String person2="assets/icon/person2.png";
-  static const String person3="assets/icon/person3.png";
-  static const String person4="assets/icon/person4.png";
-
-
-
-  static const String bookmark="assets/icon/bookmark.png";
-  static const String backArrow="assets/icon/backArrow.png";
-
-
-  static const String profileMask="assets/images/profileMask.png";
-  static const String trip="assets/icon/trip.png";
-  static const String settings="assets/icon/settings.png";
-  static const String version="assets/icon/version.png";
-
-
-  static const String dots="assets/icon/dots.png";
-  static const String edit="assets/icon/edit.png";
-  static const String check="assets/icon/check-mark.png";
-
-
-
-
+  static const String dots = "assets/icon/dots.png";
+  static const String edit = "assets/icon/edit.png";
+  static const String check = "assets/icon/check-mark.png";
 }
